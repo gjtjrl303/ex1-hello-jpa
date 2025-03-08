76d1ac235eee0796eb23a72056a242ff3e72173a
gjtjrl303 <gjtjrl303@naver.com>
2025-03-08 15:41:20 +0900

    feat: 영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,16 +15,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            Member member = em.find(Member.class, 150L);
+            member.setName("aaaaa");
 
+            em.clear();
+            Member member2 = em.find(Member.class, 150L);
+
+
+            System.out.println("=========================");
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         }finally {
